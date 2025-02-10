6cbed997aff7227366b1cf747687eabd97b77940
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-02-10 12:00:10 +0530

    Add files via upload

---
+++ birthday dates.py
@@ -0,0 +1,34 @@
+a=21
+h="hamunth"
+print(a)
+print(h)
+j=input("Enter the name of the user")
+print("Best of luck",j)
+
+g=24
+r="ramash"
+print(g)
+print(r)
+r=input("Enter the name of the user")
+print("Best of luck",r)
+
+d=32
+w="gopal"
+print(d)
+print(w)
+w=input("Enter the name of the user")
+print("Best of luck",w)
+
+s=12
+p="dave"
+print(s)
+print(p)
+p=input("Enter the name of the user")
+print("Best of luck",p) 
+
+t=40
+u="showathi"
+print(t)
+print(u)
+u=input("Enter the name of the user")
+print("Best of luck",u)
