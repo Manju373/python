d6c2eb43c83c045aa000aaea2a947c59151c0968
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-02-10 14:48:20 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+tree1= 23
+tree2= 45
+tree3= 32
+tree4=69
+tree5= 90
+sum= tree1+tree2+tree3+tree4+tree5 
+print(sum)
+average=sum/5
+print(average)
---
+++ percentage.py
@@ -0,0 +1,8 @@
+hindi = int(input ("hindi marks "))
+english = int(input("english marks "))
+math = int(input("math marks "))
+science = int(input("science"))
+sum=hindi+english+math+science
+print(sum)
+perc=(sum/400)*100
+print(perc)
