74ce88a643c893dc2cb79e47d84b4e88b68fa787
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-03-09 08:20:13 +0530

    Add files via upload

---
+++ Student age enroll.py
@@ -0,0 +1,7 @@
+name = input("Enter Student name: ")
+age = int(input("Enter Student age: "))
+
+if 10 <= age <=20:
+    print("Student name and age has been enrilled successfully")
+else:
+    print("Sorry, name only Students aged 10 to 20 can enroll.")
