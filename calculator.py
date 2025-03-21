2167f4a3725c39492b426ccaf2c8eb1474f6b038
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-03-21 14:53:41 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add(w,u):
+    return w+u
+def multiply(w,u):
+    return w*u
+w=int(input("Enter any number"))
+u=int(input("Enter any number"))
+
+print(add(w,u))
+print(multiply(w,u))
---
+++ wellwishes.py
@@ -0,0 +1,8 @@
+
+def WellWishes():
+    print("Well wishes to you!")
+    print("May you have a great day!") 
+
+
+WellWishes()
+WellWishes()
