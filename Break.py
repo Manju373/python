aa7193e0dcd3c43000c78cb52dc2d58d87ea794f
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-03-28 14:42:20 +0530

    Add files via upload

---
+++ Break.py
@@ -0,0 +1,7 @@
+a = input("Enter the name ")
+for i in a :
+    if i == "n":
+        print("N is found")
+        break
+    else: 
+        print("N is not found")
---
+++ pass.py
@@ -0,0 +1,7 @@
+for i in range(2,13):
+    if i==6:
+        pass
+    elif i==7:
+        print("buzz")
+    else:
+        print(i)
