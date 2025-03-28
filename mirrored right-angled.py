763f9dc731de5647f6736e8f7afcf6c62ae15dbc
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-03-28 11:39:17 +0530

    Add files via upload

--- mirrored right-angled.py
+++ mirrored right-angled.py
@@ -1,6 +1,4 @@
-rows = int(input("Enter the number of rows"))
+rows = 5
 
-for i in range(1,rows+2):
-    for j in range (1,i+1):
-        print("*",end=" ")
-    print()
+for i in range(1,rows+1):
+    print(" "*(rows-i)+"*"*i)
