82d32e872c2df0c23ea918fcaf7920cdaa8068b9
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-04-28 14:37:18 +0530

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,6 @@
+for i in range (1,12):
+    if i==9:
+        exit()
+    else:
+        print(i) 
+print("manju")
---
+++ map.py
@@ -0,0 +1,6 @@
+a=[1,6,3,7]
+d=[4,8,7,2]
+def add(a,d):
+    return a+d 
+s=list(map(add,a,d))
+print(s)
