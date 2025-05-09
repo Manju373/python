ed0d2e2c52c123b294836dcda3e29a8d7556ae3e
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-05-09 11:24:42 +0530

    Add files via upload

---
+++ dog.py
@@ -0,0 +1,8 @@
+class dog:
+    def __init__(self,breed,colour):
+        self.breed=breed
+        self.colour=colour
+        d1 = dog("germensufred", "black")
+        print("breed of a dog is",d1.breed)
+        print("colour of a dog is",d1.colour)
+        
