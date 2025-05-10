e2ec9dc36016b4004f96a07e43ef42c23c95d189
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-05-10 21:14:27 +0530

    Add files via upload

---
+++ Area and Perimeter.py
@@ -0,0 +1,7 @@
+class circle:
+    def __init__ (self,area,perimeter):
+        self.area=area
+        self.perimeter=perimeter
+a1= circle(2,2)
+print("area of a circle is",a1.area)
+print("perimeter of a circle is",a1.perimeter)
