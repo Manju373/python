eb12da94d6428c904cf9be3b1cc7061ca4fc382e
Manju373 <144766479+Manju373@users.noreply.github.com>
2025-06-09 14:44:45 +0530

    Add files via upload

---
+++ collision.py
@@ -0,0 +1,35 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((600,400))
+pygame.display.set_caption("collision")
+wh=(255,255,255)
+blue=(0,0,255)
+red=(255,0,0)
+p= pygame.Rect(50,50,50,50)
+o= pygame.Rect(300,200,60,60)
+run = True
+pv = True
+speed =12
+ov = True 
+while run: 
+    pygame.time.delay(30)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT :
+            pygame.quit()
+    is1 = pygame.key.get_pressed()
+    if is1[pygame.K_LEFT] :
+        p.x -= speed
+    if is1[pygame.K_RIGHT] :
+        p.x += speed
+    if is1[pygame.K_DOWN] :
+        p.y += speed
+    if is1[pygame.K_UP] :
+        p.y -= speed
+    if p.colliderect(o):
+        ov =False
+    screen.fill(wh)
+    if pv :
+        pygame.draw.rect(screen,blue,p)
+    if ov :
+        pygame.draw.rect(screen,red,o)
+    pygame.display.flip()
